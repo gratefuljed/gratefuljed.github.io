8240294f7c9489ac0344b91831767c9074f3b0d0
piflynn <piflynn@me.com>
2018-07-22 02:01:21 -0400

    fix autoplay

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  function initPlayer(sourceUrl) {
+    var audio = $("#player");
+    $("#mpeg-src").attr("src", sourceUrl);
+    /****************/
+    audio[0].pause();
+    audio[0].load(); //suspends and restores all audio element
+    audio[0].pause();
+
+  }
   function change(sourceUrl) {
     var audio = $("#player");
     $("#mpeg-src").attr("src", sourceUrl);
@@ -10,8 +19,8 @@ $(document).ready(function() {
     audio[0].oncanplaythrough = audio[0].play();
     /****************/
   }
-  change("audio/they_love_sampson.m4a");
-  $("#they_love_samson").addClass("active");
+  initPlayer("audio/they_love_sampson.m4a");
+
   $("#they_love_samson").click(function() {
     change("audio/they_love_sampson.m4a");
     $("#they_love_samson").addClass("active");
